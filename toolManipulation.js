301f4c03a0405186203d05adb5497c74418b47e7
sheikhazeem786 <sheikhazeem141@gmail.com>
2022-02-21 14:38:28 +0530

    upto pencil,eraser,rectangle,line,stickynote,color

--- toolManipulation.js
+++ toolManipulation.js
@@ -4,6 +4,7 @@ let eraser = document.querySelector("#eraser ");
 let rect = document.querySelector("#rect");
 let line = document.querySelector("#line");
 let options = document.querySelectorAll(".box-size");
+
 pencil.addEventListener("click", function(e) {
     if (currTool == "pencil") {
         //second click
@@ -17,6 +18,7 @@ pencil.addEventListener("click", function(e) {
         // pencil.style.border = "1px solid red";
         currTool = "pencil";
         tool.strokeStyle = "red";
+        tool.lineWidth = pencilSize;
     }
 })
 eraser.addEventListener("click", function(e) {
@@ -25,13 +27,14 @@ eraser.addEventListener("click", function(e) {
         //options show
         options[1].style.display = "flex";
     } else {
-        for (let i = 0; i < options.length; i++) {
-            options[i].style.display = "none";
-        }
         //first click
         // eraser.style.border = "1px solid red";
         currTool = "eraser";
         tool.strokeStyle = "white";
+        for (let i = 0; i < options.length; i++) {
+            options[i].style.display = "none";
+        }
+        tool.lineWidth = eraserSize;
     }
 })
 rect.addEventListener("click", function(e) {
@@ -47,6 +50,7 @@ rect.addEventListener("click", function(e) {
         // rect.style.border = "1px solid red";
         currTool = "rect";
         tool.strokeStyle = "red";
+        tool.lineWidth = rectSize;
     }
 })
 line.addEventListener("click", function(e) {
@@ -62,9 +66,9 @@ line.addEventListener("click", function(e) {
         //first click
         currTool = "line";
         tool.strokeStyle = "red";
+        tool.lineWidth = lineSize;
     }
 })
-
 
 // Color change logic
 let redColor = document.querySelector(".red");
@@ -85,3 +89,92 @@ blueColor.addEventListener("click",
     })
 
 //Size change logic
+let pencilSize = 5;
+let eraserSize = 5;
+let rectSize = 5;
+let lineSize = 5;
+let sizeBoxArr = document.querySelectorAll(".box-size");
+sizeBoxArr[0].addEventListener("click", function(e) {
+    let elements = ["size1", "size2", "size3", "size4"];
+    let allTheClasses = e.target.classList;
+    let firstClass = allTheClasses[0];
+    let test = elements.includes(firstClass);
+    if (test) {
+        // console.log(firstClass);
+        if (firstClass == "size1") {
+            pencilSize = 5;
+
+        } else if (firstClass == "size2") {
+            pencilSize = 10;
+
+        } else if (firstClass == "size3") {
+            pencilSize = 15;
+        } else if (firstClass == "size4") {
+            pencilSize = 20;
+        }
+    }
+    tool.lineWidth = pencilSize;
+})
+sizeBoxArr[1].addEventListener("click", function(e) {
+    let elements = ["size1", "size2", "size3", "size4"];
+    let alltheClasses = e.target.classList;
+    let firstClass = alltheClasses[0];
+    let test = elements.includes(firstClass);
+    if (test) {
+        // console.log(firstClass);
+        if (firstClass == "size1") {
+            eraserSize = 5;
+
+        } else if (firstClass == "size2") {
+            eraserSize = 10;
+
+        } else if (firstClass == "size3") {
+            eraserSize = 15;
+        } else if (firstClass == "size4") {
+            eraserSize = 20;
+        }
+    }
+    tool.lineWidth = eraserSize;
+})
+sizeBoxArr[2].addEventListener("click", function(e) {
+    let elements = ["size1", "size2", "size3", "size4"];
+    let alltheClasses = e.target.classList;
+    let firstClass = alltheClasses[0];
+    let test = elements.includes(firstClass);
+    if (test) {
+        // console.log(firstClass);
+        if (firstClass == "size1") {
+            rectSize = 5;
+
+        } else if (firstClass == "size2") {
+            rectSize = 10;
+
+        } else if (firstClass == "size3") {
+            rectSize = 15;
+        } else if (firstClass == "size4") {
+            rectSize = 20;
+        }
+    }
+    tool.lineWidth = rectSize;
+})
+sizeBoxArr[3].addEventListener("click", function(e) {
+    let elements = ["size1", "size2", "size3", "size4"];
+    let alltheClasses = e.target.classList;
+    let firstClass = alltheClasses[0];
+    let test = elements.includes(firstClass);
+    if (test) {
+        // console.log(firstClass);
+        if (firstClass == "size1") {
+            lineSize = 5;
+
+        } else if (firstClass == "size2") {
+            lineSize = 10;
+
+        } else if (firstClass == "size3") {
+            lineSize = 15;
+        } else if (firstClass == "size4") {
+            lineSize = 20;
+        }
+    }
+    tool.lineWidth = lineSize;
+})
